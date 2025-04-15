9a310a3d77cc8e90bcbf50355325f147c4b98c99
“Mayank <mayanksrivastava11111@gmail.com>
2025-04-15 19:23:06 +0530

    Shifted MongoDB to Online Server

--- db.js
+++ db.js
@@ -1,9 +1,10 @@
  const mongoose = require('mongoose');
+ require('dotenv').config();
 
  //Define the MongoDB connection URL
- //const mongoURL = 'mongodb://127.0.0.1:27017/hotels'; // Replace with your MongoDB connection string
- const mongoURL = 'mongodb+srv://mayank19:mayank%408528@cluster0.3beqby3.mongodb.net/'//written password of mongodb that is 'mayank@8528' as 'mayank%408528' in the connection string.
- 
+ //const mongoURL = process.env.MONGODB_URL_LOCAL // Replace with your MongoDB connection string
+ const mongoURL =process.env.MONGODB_URL; //written password of mongodb that is 'mayank@8528' as 'mayank%408528' in the connection string.
+
  //set up MongoDB connection 
  mongoose.connect(mongoURL, {
      useNewUrlParser: true,
@@ -29,4 +30,4 @@
     })
 
     //export the database connection
-    module.exports = db; // Export the database connection for use in other files
+    module.exports = db; // Export the database connection for use in other files 
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express();
-
 const db = require('./db') // Import the database connection
+require('dotenv').config(); // Load environment variables from .env file
+
 const bodyParser=require('body-parser'); // Import body-parser to parse request bodies
 app.use(bodyParser.json()); // Use body-parser middleware to parse JSON request bodies
+const PORT=process.env.PORT || 3000; // Set the port to the value in the .env file or default to 3000
 
 const Person=require('./models/Person'); // Import the Person model
 //const Menuitem =require('./models/MenuItem'); // Import the Menuitem model
